0e546a924a89ae85c8fae765eafe7227b0b535be
Josh Pizel <joshpizel@AD.DOE.CARLETON.CA>
2018-02-02 14:09:20 -0500

    finished main portions of all parts

--- assignment1/Main.m
+++ assignment1/Main.m
@@ -36,10 +36,34 @@ displaySize=10;
 X= rand(2,size);
 Y= rand(2,size);
 
-%positions
-X(1,:)= X(1,:)*workX;
-Y(1,:)= Y(1,:)*workY;
+%positions initialize
+Xpos(1,:)= X(1,:)*workX;
+Ypos(1,:)= Y(1,:)*workY;
 
+checkXboxleft = Xpos>0.8e-7;
+checkXboxright = Xpos<1.2e-7;
+checkXbox = checkXboxleft & checkXboxright;
+checkYBoxbottom = Ypos<0.4e-7;
+checkBoxbottom = checkYBoxbottom & checkXbox;
+checkYBoxtop = Ypos>0.6e-7;
+checkBoxtop = checkYBoxtop & checkXbox;
+
+checkboxes = checkBoxtop | checkBoxbottom;
+while(sum(checkboxes)>0)
+    
+    Xpos(checkboxes) = rand*workX;
+    Ypos(checkboxes) = rand*workY;
+    
+    checkXboxleft = Xpos>0.8e-7;
+    checkXboxright = Xpos<1.2e-7;
+    checkXbox = checkXboxleft & checkXboxright;
+    checkYBoxbottom = Ypos<0.4e-7;
+    checkBoxbottom = checkYBoxbottom & checkXbox;
+    checkYBoxtop = Ypos>0.6e-7;
+    checkBoxtop = checkYBoxtop & checkXbox;
+
+    checkboxes = checkBoxtop | checkBoxbottom;
+end
 colour = rand(1,displaySize);
 %initial direction of each particle
 angle(1,:) = X(2,:)*2*pi;
@@ -69,9 +93,6 @@ dt = spacStep/Vth;
 steps = 1000;
 
 %variable change
-%setup mapping
-Xpos = X(1,:);
-Ypos = Y(1,:);
 
 Xvel(1,:) = Xvel(1,:)*dt;
 Yvel(1,:) = Yvel(1,:)*dt;
@@ -80,6 +101,12 @@ Yvel(1,:) = Yvel(1,:)*dt;
 Pscat=1-exp(-(dt/MTBC));
 
 figure(1)
+boxplotX = [0.8e-7 0.8e-7 1.2e-7 1.2e-7];
+boxplotY = [0 0.4e-7 0.4e-7 0];
+plot(boxplotX,boxplotY,'color',[0 0 0]);
+hold on
+boxplotY = [1e-7 0.6e-7 0.6e-7 1e-7];
+plot(boxplotX,boxplotY,'color',[0 0 0]);
 %main function
 for i = 1:1:steps
     
@@ -99,17 +126,56 @@ for i = 1:1:steps
     Xpos(checkXleft) = Xpos(checkXleft) +Xvel(checkXleft)+workX;
     
     %bottle neck
-    checkXbox = (Xpos + Xvel)>0.8e-7 && (Xpos + Xvel)<1.2e-7;
+    
+    %bottom box
+    %for x reflection
+    checkXboxleft = (Xpos + Xvel)>0.8e-7;
+    checkXboxright = (Xpos + Xvel)<1.2e-7;
+    checkXbox = checkXboxleft & checkXboxright;
+    
     checkYBoxbottom =(Ypos + Yvel)<0.4e-7;
-    checkBoxBottom = checkYBoxbottom && checkXbox;
+    
+    checkBoxBottom = checkYBoxbottom & checkXbox;
     
     Xvel(checkBoxBottom) = Xvel(checkBoxBottom).*(-1);
     
-    leftover = ~(checkXright | checkXleft);
+    %for y reflection
+    checkXboxleft = (Xpos + Xvel)>0.8e-7+spacStep;
+    checkXboxright = (Xpos + Xvel)<1.2e-7-spacStep;
+    checkXbox = checkXboxleft & checkXboxright;
+    checkYabove =Ypos<0.4e-7-spacStep;
+    changeY = checkYabove & checkXbox;
+    
+    Yvel(changeY) = Yvel(changeY).*(-1);
+    
+    %Top box
+    %for x reflection
+    checkXboxleft = (Xpos + Xvel)>0.8e-7;
+    checkXboxright = (Xpos + Xvel)<1.2e-7;
+    checkXbox = checkXboxleft & checkXboxright;
+    
+    checkYBoxbottom =(Ypos + Yvel)>0.6e-7;
+    
+    checkBoxBottom = checkYBoxbottom & checkXbox;
+    
+    Xvel(checkBoxBottom) = Xvel(checkBoxBottom).*(-1);
+    
+    %for y reflection
+    checkXboxleft = (Xpos + Xvel)>0.8e-7+spacStep;
+    checkXboxright = (Xpos + Xvel)<1.2e-7-spacStep;
+    checkXbox = checkXboxleft & checkXboxright;
+    checkYabove =Ypos>0.6e-7+spacStep;
+    changeY = checkYabove & checkXbox;
+    
+    Yvel(changeY) = Yvel(changeY).*(-1);
+    
+    %leftover x 
+    leftover = ~(checkXright | checkXleft | checkBoxBottom);
     
     Xpos(leftover) = Xpos(leftover) +Xvel(leftover);
     
-    checkY = (Ypos+Yvel>1e-7 | Ypos+Yvel<0);%reflection y's
+    %reflect Y boundary
+    checkY = (Ypos+Yvel>1e-7 | Ypos+Yvel<0);
     Yvel(checkY) = Yvel(checkY).*(-1);
     Ypos(1,:) = Ypos(1,:)+Yvel(1,:);
     
@@ -124,6 +190,7 @@ for i = 1:1:steps
     prevY(i,:) =Ypos(1,:);
     for j = 1:1:displaySize
         plot(prevX(:,j),prevY(:,j),'color',[colour(1,j) 0 j/displaySize])
+        
         xlim([0 workX])
         ylim([0 workY])
         legend(['Temperature:' num2str(averageTemp)])
